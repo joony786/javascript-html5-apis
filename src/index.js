900231dfc349e4780e81bd20e906d96470ce925a
joony786 <joony786@gmail.com>
2021-08-24 06:45:34 +0500

    2-declaring draggable comp

--- src/index.js
+++ src/index.js
@@ -2,8 +2,40 @@ import '../assets/css/style.css';
 
 const app = document.getElementById('app');
 app.innerHTML = `<h1>JavaScript HTML5 APIs</h1> 
-<div draggable></div> 
-<div class='dropZone'> drop here</div>`;
+<div class='uploader'> 
+<div class='dragme' draggable='true'></div>
+<div class='dropzone'> drag here!</div> 
+</div>
+
+<style>
+.uploader{
+    box-sizing: border-box;
+    max-width: 90%;
+    border-radius: 10px;
+    border-bottom: 3px solid #d2d5da;
+    margin: 25px auto;
+    padding: 25px;
+    background : #fff
+}
+
+.dragme {
+    background: #ce1f;
+    border-radius: 5px;
+    width: 50px;
+    height: 50px;
+}
+.dropzone {
+    border-radius: 5px;
+    margin-top: 25px;
+    padding: 25px;
+    border: 2px dashed #d2d;
+    background: #f1f2f5
+}
+.active {
+    background: #ce1
+}
+</style>
+`;
 
 const init = () => {}
 
