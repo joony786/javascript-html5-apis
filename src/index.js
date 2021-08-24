1ccdadfc67a8ecb15a0a35f7568825f1fd341a6d
joony786 <joony786@gmail.com>
2021-08-24 06:25:07 +0500

    1-draggable feat. detect

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import '../assets/css/style.css';
 
 const app = document.getElementById('app');
-app.innerHTML = '<h1>JavaScript HTML5 APIs</h1>';
+app.innerHTML = `<h1>JavaScript HTML5 APIs</h1> 
+<div draggable></div> 
+<div class='dropZone'> drop here</div>`;
+
+const init = () => {}
+
+console.dir(document.createElement('div'))
+
+if('draggable' in document.createElement('div')){
+    init()
+}
+
